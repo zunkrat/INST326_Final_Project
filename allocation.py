9a046736b06790316bc898c234b452e188eeef9b
Gwon Lee <twojunk49@gmail.com>
2023-11-10 13:19:39 -0500

    added a comment

--- allocation.py
+++ allocation.py
@@ -1,1 +1,28 @@
 categories = ['entertainment', 'groceries', 'other','travel']
+
+def __init__ (self, total_income, checking_percent, savings_percent, bank_accounts):
+
+    self.bank_accounts = bank_accounts
+    self.total_income = total_income
+    self.checking_percent = checking_percent
+    self.savings_percent = savings_percent
+
+def calculator(self): # Gwon and Christian
+
+    money_to_checking = self.total_income * self.checking_percent
+
+    # budgetings such as groceries, etc from checking account.
+    
+    money_to_savings = self.total_income * self.savings_percent
+
+    # budgetings such as groceries, etc from checking account.
+
+    if isinstance(money_to_checking, int):
+        checking_balance = self.bank_accounts["checking"]
+        checking_balance += money_to_checking
+        self.bank_accounts["checking"] = checking_balance
+    
+    if isinstance(money_to_savings, int):
+        savings_balance = self.bank_accounts["savings"]
+        savings_balance += money_to_savings
+        self.bank_accounts["savings"] = savings_balance
