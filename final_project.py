4f274e8282cf849c37233c6a54e4dc94c420c2e5
Gwon Lee <143454114+zunkrat@users.noreply.github.com>
2023-12-10 13:16:38 -0500

    added docsting to my code(Gwon)

--- final_project.py
+++ final_project.py
@@ -24,19 +24,56 @@ class PayStubExtraction:
         self.net_pay = extracted_info.get('Net Pay', '')
 
 class IncomeAllocator:
+    """Class for managing income allocation to checking and savings accounts.
+    
+    Attributes:
+        total_income (float): Total income amount.
+        checking_percent (float): Percentage of the total income allocated to the checking account.
+        savings_percent (float): Percentage of the total income allocated to the savings account.
+        bank_accounts (dict): Dictionary to store allocated amounts in checking and savings accounts.        
+    """
+    
     def __init__(self, pay_stub_info, checking_percent, savings_percent):
+        """
+        Initialize the IncomeAllocator object.
+
+        Args:
+            total_income (float): Total income amount.
+            checking_percent (float): Percentage of the total income allocated to the checking account.
+            savings_percent (float): Percentage of the total income allocated to the savings account.
+            bank_accounts (dict): Dictionary to store allocated amounts in checking and savings accounts.
+            
+        Side effects:
+            Create IncomeAllocator attributes.
+        """
+        
         self.bank_accounts = {}
         self.total_income = float(pay_stub_info.net_pay)
         self.checking_percent = checking_percent
         self.savings_percent = savings_percent
 
     def check_percentage(self):
+        """Check if the percentage is valid
+        
+        Returns:
+            float: allocating percentage to checking and savings account.
+            
+        Raise:
+            ValueError: Percentage has to be within 0 to 1.
+        """
+        
         if 0 <= self.checking_percent <= 1 and 0 <= self.savings_percent <= 1:
             return self.checking_percent, self.savings_percent
         else:
             raise ValueError("Percentage has to be within 0 to 1")
 
     def calculator(self):
+        """Calculate and allocate income to checking and savings accounts.
+        
+        Returns:
+            dict: the deposit amount to checking and savings account.
+        """
+        
         self.check_percentage()
         money_to_checking = round((self.total_income * self.checking_percent), 2)
         money_to_savings = round((self.total_income * self.savings_percent), 2)
@@ -47,6 +84,17 @@ class IncomeAllocator:
         return self.bank_accounts
 
 def main():
+    """Calculate the amount to deposit into checking and savings account using the percentage from users and income read from the paystub.
+    
+    Args:
+        checking_percent: percentage set by the user to allocate income to checking account.
+        savings_percent: percentage set by the user to allocate income to savings account.
+        income (float): Total income amount obtained from the paystub. A default value obtained from the paystub using the get_income_from_paystub() function.
+    
+    Returns:
+        dict: the deposit amount to checking and savings account.
+    """
+    
     args = parse_args(sys.argv[1:])
     pay_stub_info = PayStubExtraction(args.filepath)
     income_allocator = IncomeAllocator(pay_stub_info, args.checking_percent, args.savings_percent)
@@ -54,6 +102,20 @@ def main():
     return deposit
 
 def parse_args(arglist):
+    """ Parse command-line arguments.
+    
+    Expect three mandatory arguments:
+        - filepath: path to the file.
+        - checking_percent: percentage set by the user to allocate income to checking account.
+        - savings_percent: percentage set by the user to allocate income to savings account.
+    
+    Args:
+        arglist (list of str): arguments from the command line.
+    
+    Returns:
+        namespace: the parsed arguments, as a namespace.
+    """
+    
     parser = ArgumentParser()
     parser.add_argument("filepath", type = str, help = "Path to the pay stub file", default="converted_pay_stub.txt")
     parser.add_argument("checking_percent", type = float, help = "Percentage for checking")
