619c68503da9a2c2045de4b5e93cb02d29df7e5b
Gwon Lee <143454114+zunkrat@users.noreply.github.com>
2023-12-10 21:57:17 -0500

    deleted unnecessary files

--- final_project.py
+++ final_project.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import sys
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class PayStubExtraction:
     """Class that extracts specific information from an uploaded paystub.
@@ -37,13 +38,6 @@ class PayStubExtraction:
         'Net Pay': r'Net Pay\s*[\s\S]*?(\d+,\d+\.\d{2})'
     }
 
-#Using PayStubExtraction to extract information from a paystub
-file_path = "11_4_23_paystub.txt" 
-with open(file_path, 'r') as file:
-    text_content = file.read()
-
-extracted_info = {key: re.search(pattern, text_content).group(1) for key, pattern in PayStubExtraction.patterns.items() if re.search(pattern, text_content)}
-
 class IncomeAllocator:
     """Class for managing income allocation to checking and savings accounts.
     
@@ -70,23 +64,24 @@ class IncomeAllocator:
         
         self.bank_accounts = {}
         self.total_income = float(pay_stub_info.net_pay)
-        self.checking_percent = checking_percent
-        self.savings_percent = savings_percent
 
-    def check_percentage(self):
-        """Check if the percentage is valid
-        
-        Returns:
-            float: allocating percentage to checking and savings account.
-            
-        Raise:
-            ValueError: Percentage has to be within 0 to 1.
-        """
-        
-        if 0 <= self.checking_percent <= 1 and 0 <= self.savings_percent <= 1:
-            return self.checking_percent, self.savings_percent
+        if 0 <= checking_percent <= 1 and 0 <= savings_percent <= 1:
+            self.checking_percent = checking_percent
+            self.savings_percent = savings_percent
         else:
             raise ValueError("Percentage has to be within 0 to 1")
+
+    def check_percentage(self):
+        """Check if the percentage is added up to 1
+        
+        Raises:
+            ValueError: Percentage has to be within 0 to 1.
+        """
+        if self.checking_percent + self.savings_percent == 1:
+            self.checking_percent = self.checking_percent
+            self.savings_percent = self.savings_percent
+        else:
+            raise ValueError("Checking percent and savings percent must equal to 1")
 
     def calculator(self):
         """Calculate and allocate income to checking and savings accounts.
@@ -103,6 +98,78 @@ class IncomeAllocator:
         self.bank_accounts["savings"] = money_to_savings if isinstance(money_to_savings, float) else float(money_to_savings)
 
         return self.bank_accounts
+    
+    def user_allocation(self):
+        """
+        Display a list of available categories and prompt the user to allocate a percentage
+        for each category. 
+
+        Returns:
+            dict: A dictionary containing the allocated percentages for each category.
+                  The keys are category names, and the values are the corresponding percentages.
+        """
+        categories = frozenset(['Entertainment', 'Groceries', 'Housing', 'Utilities', 'Travel', 'Recreation', 'Transportation', 'Other'])
+        allocation_percentages = {}
+
+        print("Available Categories:")
+        print(", ".join(categories))
+
+        allocate = input("Do you want to allocate money to any of the above categories? Enter 'yes' (or anything else to exit): ")
+
+        if allocate.lower() == 'yes':
+            checking_amount = round((self.total_income * self.checking_percent), 2)
+            print(f"Total amount available in checking based on checking percent ({self.checking_percent * 100}%): {checking_amount}")
+            print("You will enter a percentage for each category. The '%' symbol is not required.")
+            total_percentage = 0
+
+            while total_percentage < self.checking_percent * 100:
+                category = input("Enter the category you want to add to (or 'end' to finish): ").capitalize()
+
+                if category.lower() == 'end':
+                    break
+
+                if category in categories:
+                    remaining_percentage = self.checking_percent * 100 - total_percentage
+                    print(f"Remaining percentage: {remaining_percentage}%")
+                    percentage = self.get_percentage_input(f"Enter a percentage for {category}: ")
+
+                    if total_percentage + percentage > self.checking_percent * 100:
+                        print(f"Total percentage will exceed the checking percent ({self.checking_percent * 100}%). Current total: {total_percentage}%")
+                    else:
+                        allocation_percentages[category] = percentage
+                        total_percentage += percentage
+                else:
+                    print("Invalid category. Please choose from the available categories.")
+
+
+            print("Allocations:")
+            for category, percentage in allocation_percentages.items():
+                allocated_amount = round(((percentage / 100) * checking_amount), 2)
+                print(f"{category}: {percentage}% - Allocated Amount: {allocated_amount}")
+
+        return allocation_percentages
+    
+    def get_percentage_input(self, prompt):
+        """
+        Helper function to get a valid percentage input from the user.
+
+        Args:
+            prompt (str): The prompt to display.
+
+        Returns:
+            float: The valid percentage entered by the user.
+        """
+        while True:
+            try:
+                percentage = input(prompt)
+                percentage = float(percentage.rstrip('%'))
+
+                if 0 <= percentage <= 100:
+                    return percentage
+                else:
+                    print("Enter a valid number between 0 and 100.")
+            except ValueError:
+                print("Invalid input. Enter a number between 0 and 100.")
 
 def main():
     """Calculate the amount to deposit into checking and savings account using the percentage from users and income read from the paystub.
@@ -116,10 +183,55 @@ def main():
         dict: the deposit amount to checking and savings account.
     """
     
-    args = parse_args(sys.argv[1:])
-    pay_stub_info = PayStubExtraction(args.filepath)
-    income_allocator = IncomeAllocator(pay_stub_info, args.checking_percent, args.savings_percent)
+    if len(sys.argv) > 1:
+        args = parse_args(sys.argv[1:])
+        filepath = args.filepath
+        checking_percent = args.checking_percent
+        savings_percent = args.savings_percent
+    else:
+        # Gets user inputs 
+        filepath = input("Enter the path to your paystub file: ")
+        checking_percent = float(input("Enter the percentage to allocate to the checking account (0-100): ")) / 100
+        savings_percent = float(input("Enter the percentage to allocate to the savings account (0-100): ")) / 100
+
+        # Validates the input percentages
+        if not 0 <= checking_percent <= 1 or not 0 <= savings_percent <= 1:
+            raise ValueError("Percentages must be between 0 and 100")
+        
+    #Processes the pay stub    
+    with open(filepath, 'r') as file:
+        text_content = file.read()
+    extracted_info = {key: re.search(pattern, text_content).group(1) for key, pattern in PayStubExtraction.patterns.items() if re.search(pattern, text_content)}
+    
+    #Gets rid of commas in the numbers as strings
+    extracted_info['Current Earnings'] = extracted_info['Current Earnings'].replace(',', '')
+    extracted_info['Current Taxes'] = extracted_info['Current Taxes'].replace(',', '')
+    extracted_info['Net Pay'] = extracted_info['Net Pay'].replace(',', '')
+         
+    #Creates the PayStubExtraction object
+    pay_stub_info = PayStubExtraction(
+        direct_deposit_date = extracted_info['Direct Deposit Date'],
+        employee_full_name = extracted_info['Employee Full Name'],
+        current_earnings = extracted_info['Current Earnings'],
+        current_taxes = extracted_info['Current Taxes'],
+        net_pay = extracted_info['Net Pay']
+    )
+    
+    income_allocator = IncomeAllocator(pay_stub_info, checking_percent, savings_percent)
     deposit = income_allocator.calculator()
+    deposit['deposit date'] = pay_stub_info.direct_deposit_date
+    
+    print(f"Allocated amounts: {deposit}")
+    income_allocator.user_allocation()
+
+    dic_csv(deposit)
+    
+    try:
+        recent_income()
+    
+    except Exception as e:
+        print(f"Error {e}")
+
     return deposit
 
 def parse_args(arglist):
@@ -145,22 +257,66 @@ def parse_args(arglist):
     return parser.parse_args(arglist)
 
 def dic_csv(dictionary):
-    """Transforms the dictionary of the bank accounts into a csv file.
-    
+    """Appends the dictionary of the bank accounts to a CSV file.
+
     Args:
         dictionary: bank account dictionary (checking account and savings account).
-    
+
     Side effects:
-        csv_file: bank csv file that contain checking account and savings account balance will be created.
+        csv_file: bank csv file that contains checking account and savings account balances will be created or appended.
     """
-    
     df = pd.DataFrame([dictionary])
-    df.to_csv('bank.csv', index = False)
+
+    csv_file_path = 'bank.csv'
+
+    try:
+        with open(csv_file_path, 'r', newline = '') as file:
+            existing_data = file.read()
+
+        if not existing_data:
+            df.to_csv(csv_file_path, index = False)
+            
+        else:
+            with open(csv_file_path, 'a', newline = '') as file:
+                df.to_csv(file, index = False, header = False)
+
+    except Exception as e:
+        print(f"Error writing to CSV file: {e}")
+                
+def recent_income(csv_file = 'bank.csv', num_rows = 10):
+    """
+    Plot the sum of checking and savings for the specified number of most recent paystubs from a CSV file.
+
+    Args:
+        csv_file (str): Path to the CSV file containing paystub data.
+        num_rows (int): Number of most recent paystubs to include in the plot.
+
+    Returns:
+        None
+    """
+    try:
+        df = pd.read_csv(csv_file)
+    except FileNotFoundError:
+        print(f"File {csv_file} not found. Creating a new file.")
+        df = pd.DataFrame(columns=['checking', 'savings', 'deposit date'])
+        df.to_csv(csv_file, index=False)
+
+    df['Net Pay'] = df['checking'] + df['savings']
     
+    selected_columns = ['deposit date', 'Net Pay']
+    df = df[selected_columns]
+    df['deposit date'] = pd.to_datetime(df['deposit date'], format = '%m-%d-%Y')
+    df = df.sort_values(by = 'deposit date', ascending = False)
+    recent_rows = df.head(num_rows)
+
+    plt.figure(figsize = (10, 6))
+    plt.bar(recent_rows['deposit date'], recent_rows['Net Pay'], color = 'blue')
+    plt.xlabel('Deposit Date')
+    plt.ylabel('Net Pay (Checking + Savings)')
+    plt.title(f'Sum of Checking and Savings for the Last {num_rows} Deposits')
+    plt.xticks(rotation = 45)
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
-    pay_stub_info = PayStubExtraction(args.filepath)
-    income_allocator = IncomeAllocator(pay_stub_info, args.checking_percent, args.savings_percent)
-    deposit = income_allocator.calculator()
-    
-    dic_csv(deposit)
+    main()
